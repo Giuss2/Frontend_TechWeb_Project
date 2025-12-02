ffc5a807df7be8905fdfbe1dd8f2c6f5ecd1559b
Giuss2 <120112582+Giuss2@users.noreply.github.com>
2025-12-02 14:57:34 +0100

    homepage+logout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+//import { LoginComponent } from './login/login.component';
+//import { LogoutComponent } from './logout/logout.component';
+//import { ProfileComponent } from './profile/profile.component';
+//import { OtherProfileComponent } from './other-profile/other-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  //{ path: 'profile', component: ProfileComponent },
+  //{ path: 'profile/:id', component: OtherProfileComponent },
+  { path: '**', redirectTo: '' } // wildcard, redirect alla home se URL non trovato
+];
