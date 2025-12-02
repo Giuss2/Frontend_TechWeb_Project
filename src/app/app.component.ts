1d1a0671bc9d5b24212c6ddb5e8d5dbea276b24e
Giuss2 <120112582+Giuss2@users.noreply.github.com>
2025-12-02 14:42:18 +0100

    navbar+homepage (working in progress)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,56 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, AfterViewInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './navbar/navbar';
+import { CommonModule } from '@angular/common';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Navbar, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  protected readonly title = signal('angular-app');
+
+export class AppComponent implements AfterViewInit{
+  private map!: L.Map;
+  constructor(){}
+  showWelcomeMessage = signal(true);
+
+  ngOnInit() {
+    const seen = localStorage.getItem('seenWelcome');
+    this.showWelcomeMessage.set(!seen);
+  }
+
+  closeWelcomeMessage() {
+    this.showWelcomeMessage.set(false);
+    localStorage.setItem('seenWelcome', 'true');
+  }
+
+   ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    // Centra la mappa su una posizione iniziale (es. Italia)
+    this.map = L.map('map', {
+      center: [41.9028, 12.4964], // lat, lng (Roma)
+      zoom: 6
+    });
+
+    // Aggiunge i tiles di OpenStreetMap
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(this.map);
+
+    setTimeout(() => {
+      this.map.invalidateSize();
+      }, 100);
+
+     // aggiungere un marker
+    L.marker([41.9028, 12.4964])
+      .addTo(this.map)
+      .bindPopup('Gatto segnalato qui 🐾');
+  }
+
 }
