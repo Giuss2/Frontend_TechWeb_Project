c9c1c382b08df6b685291c3314a0da1759cd89a6
Giuss2 <120112582+Giuss2@users.noreply.github.com>
2025-12-03 15:34:13 +0100

    login+logout

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth-service/auth-service';
 import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.scss']
 })
 export class Navbar {
-  constructor(public auth: AuthService) {}
+  constructor(public auth: AuthService, private router: Router) {}
+
+  login() {
+    this.router.navigate(['/login']);
+  }
+
+  logout() {
+    this.auth.logout();           
+    this.router.navigate(['/']);  
+  }
 }
